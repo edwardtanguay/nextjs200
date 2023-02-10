ced5cadba8aabf22f05f3b327e39d8c28e52f3e7
Edward Tanguay (windows) <edward@tanguay.info>
2023-02-10 02:12:57 +0100

    css in

---
+++ app/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles.css'
+
+function Header() {
+	return (
+		<header className="theHeader">
+			<p>This is the header</p>
+		</header>
+	);
+}
+
+export default Header;
